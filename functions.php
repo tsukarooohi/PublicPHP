eb5575edebf74d4f1ac744b32268b207d29cecce
tsukarooohi <tsukarooohi@gmail.com>
2012-04-05 12:57:58 +0900

    com

--- functions.php
+++ functions.php
@@ -1,17 +1,28 @@
 <?php
 
-function h($string, $encode='utf-8'){
+function h( $string, $encode='utf-8', $type='' ){
 
-	return htmlspecialchars($string, ENT_QUOTES, $encode);
+	if( $type) return htmlspecialchars_decode( $string, ENT_QUOTES );
+
+	return htmlspecialchars( $string, ENT_QUOTES, $encode );
 }
 
-function m($string){
-	return mysql_real_escape_string($string);
+function m( $string ){
+	
+	return mysql_real_escape_string( trim( $string ) );
 }
 
-function Location($url){
+function u( $string, $type='' ){
 
-	header("location: ".$url);
+	if( $type )
+		return urldecode( $string );
+	else
+		return urlencode( $string );
+}
+
+function Location( $url ){
+
+	header( 'location: '.$url );
 	exit;
 }
 
@@ -21,26 +32,26 @@ function Location($url){
  * $text: 書き込み内容
  * $type: 書き込みﾀｲﾌﾟ
  */
-function file_put($dir, $string, $type=''){
+function file_put( $dir, $string, $type='' ){
 
 	$version = phpversion();
-	if((substr($version, 0, 1)) == '5'){
+	if( ( substr( $version, 0, 1 ) ) ===  5 ){
 
-		if($type == 'a') file_put_contents($dir, $string, FILE_APPEND);
-		else file_put_contents($dir, $string);
+		if( $type === 'a' ) file_put_contents( $dir, $string, FILE_APPEND );
+		else file_put_contents( $dir, $string );
 
 	}else{
 
-		$fp = fopen($dir, $type);
-		fwrite($fp, $string);
-		fclose($fp);
+		$fp = fopen( $dir, $type );
+		fwrite( $fp, $string );
+		fclose( $fp );
 	}
-	chmod($dir, 0777);
+	chmod( $dir, 0777 );
 }
 
-function mb($str, $to, $from){
+function mb( $string, $to, $from ){
 
-	return mb_convert_encoding($str, $to, $from);
+	return mb_convert_encoding( $string, $to, $from );
 }
 
 // クラスインスタンスの生成処理
